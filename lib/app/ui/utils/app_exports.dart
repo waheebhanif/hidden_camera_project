27f9496952854a24ca1d2d60c4030e526339e799
muhammadzaheeratitsol <109297508+MuhammadZaheer57886@users.noreply.github.com>
2024-12-22 16:14:13 +0500

    code clean up

--- lib/app/ui/utils/app_exports.dart
+++ lib/app/ui/utils/app_exports.dart
@@ -7,5 +7,4 @@ export 'package:get/get.dart';
 export 'package:camera/camera.dart';
 export 'package:hidden_camera_detector/app/controllers/home_controller.dart';
 // export 'package:hidden_camera_detector/app/ui/pages/scanner/scanner_controller.dart';
-export 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 export 'package:sensors_plus/sensors_plus.dart';
